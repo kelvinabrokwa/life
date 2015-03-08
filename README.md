ade7ef0e890ac8c02f700b1ea0ad9d1dfaac6280
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-03-07 19:11:01 -0500

    working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# life
-Place holder repo for game of life implementation
+# [life](http://abrokwa.org/life)
+
+Simple JavaScript and HTML canvas implementation of [Conway's Game of Life](http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life). Currently starts with a randomly generated seed.
+
+###To do
+* Implement color gradient representing cell longevity
+* Write some custom seeds
+* Interactivity:
+ * Slider for choosing regeneration speed
+ * Ability to choose seed
+ * Pause, and regenerate by button
--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-/**
- * To do:
- *    Implement color gradient for cell longevity
- */
-var size = 10; // in pixels
-var cell_num = 60;
+var size = 12;
+var cell_num = 70;
 
 var c = document.getElementById('zone');
 c.width = size*cell_num;
 c.height = size*cell_num;
-var ctx = c.getContext("2d");
-
-function drawGrid(x,y,s) {
-  ctx.beginPath();
-  ctx.rect(x*size, y*size, s, s);
-  ctx.lineWidth = '0.3';
-  ctx.stroke();
-}
-
-for (var i=0; i<cell_num; i++) {
-  for (var j=0; j<cell_num; j++) {
-      drawGrid(i, j, size);
-  }
-}
+var ctx = c.getContext('2d');
 
 var cell = [];
 
@@ -29,47 +12,39 @@ var cell = [];
   for (var i=0; i<cell_num; i++) {
     var inner = [];
     for (var j=0; j<cell_num; j++) {
-      inner.push(false)
+      inner.push(false);
     }
-    cell.push(inner)
+    cell.push(inner);
   }
 })();
 
 function findNeighbors(x,y) {
-  if ((x>0) && (y>0)) {
-    return [ [x,y-1], [x-1,y], [x-1,y+1], [x-1,y-1],
-             [x,y+1], [x+1,y], [x+1,y-1], [x+1,y+1] ];
-  } else if ((x>0) && (y===0)) {
-    return [ [x-1,y], [x-1,y+1], [x+1,y], [x+1,y+1], [x,y+1] ];
-  } else if ((x===0) && (y>0)) {
-    return [ [x,y-1], [x+1,y-1], [x,y+1], [x+1,y+1], [x+1,y] ];
-  } else if ((x===0) && (y===0)) {
-    return [ [x+1,y], [x,y+1], [x+1,y+1] ];
-  } else {
-    console.log('idk what happened')
-  }
+  function up(_x) { return _x===0 ? cell_num-1 : _x-1; }
+  function down(_x) { return _x===cell_num-1 ? 0 : _x+1; }
+  function left(_y) { return _y===0 ? cell_num-1 : _y-1; }
+  function right(_y) { return _y===cell_num-1 ? 0 : _y+1; }
+  return [ [ up(x)   ,   right(y)  ],
+           [ up(x)   ,   y         ],
+           [ up(x)   ,   left(y)   ],
+           [ x       ,   left(y)   ],
+           [ x       ,   right(y)  ],
+           [ down(x) ,   left(y)   ],
+           [ down(x) ,   y         ],
+           [ down(x) ,   right(y)  ] ];
 }
 
 function adjustStatus() {
-  console.log('this is new')
   for (var x=0, len=cell.length; x<len; x++) {
     for (var y=0, len2=cell[x].length; y<len2; y++) {
-      var live_neighbors = 0
+      var live_neighbors = 0;
       findNeighbors(x,y).forEach(function(i) {
         if (cell[i[0]][i[1]]) live_neighbors++;
       });
       if ((cell[x][y]) && ((live_neighbors < 2) || (live_neighbors > 3))) cell[x][y] = false;
-      else if (!cell[x][y] && (live_neighbors === 3)) cell[x][y] = true
+      else if (!cell[x][y] && (live_neighbors === 3)) cell[x][y] = true;
     }
   }
   generateNext();
-}
-
-function updateState(x,y,opt) { // 'kill' or revive
-  ctx.beginPath();
-  ctx.rect(x*size, y*size, size, size);
-  ctx.fillStyle = opt == 'revive' ? 'black' : 'white'
-  ctx.fill();
 }
 
 function generateNext() {
@@ -79,6 +54,13 @@ function generateNext() {
       else updateState(x,y,'kill');
     }
   }
+}
+
+function updateState(x,y,opt) {
+  ctx.beginPath();
+  ctx.rect(x*size, y*size, size, size);
+  ctx.fillStyle = opt == 'revive' ? 'black' : 'white';
+  ctx.fill();
 }
 
 var seed = {};
@@ -92,6 +74,7 @@ seed.random = function(start_num) {
     cell[x][y] = true;
   }
   generateNext();
-}
+};
 
-seed.random(1000)
+seed.random(700);
+window.setInterval(adjustStatus, 300)
